44db80b590fe8c4567c8a83920a127bd078f96fd
Guilherme Barbosa Pereira <guilherme.pereira@sorteonline.com.br>
2020-08-16 16:46:16 -0300

    Add CREATE/READ/UPDATE/DELETE report operations

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const mongodb = require('mongodb');
-const connectToDB = require('./db');
+const bodyParser = require('body-parser');
 
+const connectToDB = require('./db');
 
 // Environment variables
 const ENV = process.env.ENV || 'development';
@@ -11,13 +12,67 @@ const port = process.env.PORT || 3000;
 
 // Setup stuff
 const app = express();
-connectToDB();
+app.use(bodyParser.json());
+let database;
 
-
-app.get('*', function (_req, res) {
-    res.send('Hello there!');
+connectToDB().then((db) => {
+    database = db;
+    app.listen(port, function () {
+        console.log(`Listening on port ${port}`);
+    });
 });
 
-app.listen(port, function () {
-    console.log(`Listening on port ${port}`);
+function reportsCollection() {
+    return database.collection('report');
+}
+
+app.get('/report', async function (_req, res) {
+    const report = await reportsCollection().find().toArray();
+    res.json(report);
+});
+
+app.post('/report', async function (req, res) {
+    try {
+        const result = await reportsCollection().insertOne(req.body);
+        res.json(result.ops);
+    } catch {
+        res.status(400);
+        res.send('Could not insert the report');
+    }
+});
+
+app.put('/report/:id', async function (req, res) {
+    try {
+        const filterQuery = { _id: mongodb.ObjectID(req.params.id) };
+        const updateQuery = { $set: req.body };
+        const result = await reportsCollection().updateOne(filterQuery, updateQuery);
+        const didFindReport = result?.result?.n > 0;
+
+        if (didFindReport) {
+            res.send('Successfully edited report');
+        } else {
+            res.send('Could not find the report');
+        }
+    } catch {
+        res.status(400);
+        res.send('Could not edit the report');
+    }
+});
+
+app.delete('/report/:id', async function (req, res) {
+    try {
+        const filterQuery = { _id: mongodb.ObjectID(req.params.id) };
+        const result = await reportsCollection().deleteOne(filterQuery);
+        const didFindReport = result?.result?.n > 0;
+        
+        if (didFindReport) {
+            res.send('Successfully deleted the report');
+        } else {
+            res.status(400);
+            res.send('Could not find the report');
+        }
+    } catch {
+        res.status(400);
+        res.send('Could not delete the report');
+    }
 });
--- src/db.js
+++ src/db.js
@@ -1,20 +1,18 @@
 const mongodb = require('mongodb');
 
-module.exports = async function connectToDB () {
+module.exports = async function connectToDB() {
     const { DB_USERNAME, DB_PASSWORD, DB_NAME, DB_CLUSTER_URL } = process.env;
     const URI = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@${DB_CLUSTER_URL}?retryWrites=true&w=majority`;
-    const client = new mongodb.MongoClient(URI);
+    const client = new mongodb.MongoClient(URI, { useUnifiedTopology: true });
 
     try {
         await client.connect();
         const database = client.db(DB_NAME);
-        const collection = database.collection('report');
-        
         console.log('Connected to database! 🥳');
-    }  catch(error) {
+
+        return database;
+    } catch (error) {
         console.error('Oops, could not connect to database. 😕');
-        console.error('Stacktrace: ', error)
-    } finally {
-        client.close();
+        console.error('Stacktrace: ', error);
     }
-}
+};
